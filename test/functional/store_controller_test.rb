84df970c151bf0c483affa3905b8e470464a8c5c
maximuze <arinv@sbcglobal.net>
2013-04-16 23:04:00 +0800

    Add AJAX

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class StoreControllerTest < ActionController::TestCase
+  
   test "should get index" do
     get :index
     assert_response :success
@@ -9,5 +10,6 @@ class StoreControllerTest < ActionController::TestCase
     assert_select 'h3', 'Programming Ruby 1.9'
     assert_select '.price', /\$[,\d]+\.\d\d/
   end
+
 
 end
