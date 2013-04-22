8fa4bdeb3a3068f6459e9ef85228dec576c905f3
maximuze <arinv@sbcglobal.net>
2013-04-22 22:09:55 +0800

    Update assets for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Depot::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -26,7 +26,9 @@ Depot::Application.configure do
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
+	
+	config.action_dispatch.x_sendfile_header = nil
+		
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
@@ -65,5 +67,5 @@ Depot::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  config.assets.precompile += ['depot.css']
+  config.assets.precompile += ['depot.css', 'application.js', 'application.css']
 end
